a020af56df039ccda6313ba021db67d2bc3c51f1
Banee Ishaque K <baneeishaque2@gmail.com>
2022-09-09 02:16:51 +0530

    KMP to 1.7.20-RC

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    kotlin("multiplatform").version("1.7.20-Beta").apply(false)
+    kotlin("multiplatform").version("1.7.20-RC").apply(false)
     id("com.android.library").version("7.2.0").apply(false)
 }
